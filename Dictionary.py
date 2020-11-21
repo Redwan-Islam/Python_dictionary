e633c74d0fbe786681c8622061240e32ebe0744c
Redwan-Islam <74811459+Redwan-Islam@users.noreply.github.com>
2020-11-21 14:27:15 +0600

    Initial commit

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+print("Welcome to Dictionary")
+Dictionary={"abase":" cause to feel shame", "benefit":"advantage", "callow":"young and inexperienced", "dally":"waste time", "endear":"make attractive"}
+print("The required meaning is:")
+Search=input()
+print(Dictionary[Search])
+print("Thanks for using Dictionary")
---
+++ README.md
@@ -0,0 +1,2 @@
+# Py_dictionary
+ It is a simple dictionary in python. where you can add various kinds of word and meaning. In this dictionary user can search word. If the word is mattched with your defined set of word then its show the meaning of the word.
